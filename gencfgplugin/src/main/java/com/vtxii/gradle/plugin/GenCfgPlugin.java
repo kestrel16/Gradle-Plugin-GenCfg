2436b95ede1c5bd658b7860af0e541eb6075e39a
andyabrams <andyabrams@andys-air.fios-router.home>
2020-03-07 10:41:33 -0500

    Added documentation

--- gencfgplugin/src/main/java/com/vtxii/gradle/plugin/GenCfgPlugin.java
+++ gencfgplugin/src/main/java/com/vtxii/gradle/plugin/GenCfgPlugin.java
@@ -3,6 +3,12 @@ package com.vtxii.gradle.plugin;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 
+/**
+ * Implements Plugin interface, referencing the Task class GenCfgTask.
+ * 
+ * @author andyabrams
+ *
+ */
 public class GenCfgPlugin implements Plugin<Project>{
 
 	@Override
@@ -10,5 +16,4 @@ public class GenCfgPlugin implements Plugin<Project>{
         project.getTasks().create("gencfgTask", GenCfgTask.class);
 		
 	}
-
 }
